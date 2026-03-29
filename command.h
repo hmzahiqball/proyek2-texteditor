53404e868cd159da0d5f831760fe5572668844f2
taniadwip <taniadwip15@gmail.com>
2026-03-29 21:16:28 +0700

    Update : Membuat file command.c dan command.h untuk keperluan menyimpan perintah yang dapat di eksekusi oleh text editor

---
+++ command.h
@@ -0,0 +1,8 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+// Fungsi utama untuk memproses perintah string (misal: "./edit", "./save")
+// Menerima input string dari user dan menjalankan fungsi yang sesuai
+void executeCommand(char *command);
+
+#endif
