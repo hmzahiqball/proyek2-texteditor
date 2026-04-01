8dabb7e676ee7e42d5ad898f569a05a817cfd068
Neysavelda <neztvelda@gmail.com>
2026-04-01 21:28:24 +0700

    feat: implementasi open file pada status bar (Neysa)

--- file_io.c
+++ file_io.c
@@ -1,44 +1,54 @@
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
 #include "file_io.h"
 #include "buffer.h"
+
+//Deklarasi variabel global untuk status modifikasi dan nama file saat ini
+int is_modified = 0; 
+char current_filename[256] = "Untitled";
 
 // Menyimpan isi buffer ke file .txt (buffer ke disk)
 void saveToFile(const char *filename) {
     FILE *fp = fopen(filename, "w");
-
     if (fp == NULL) {
         printf("Gagal menyimpan ke %s.\n", filename);
         return;
     }
 
-    int i;
-    for (i = 0; i < total_lines; i++) {
+    for (int i = 0; i < total_lines; i++) {
         fprintf(fp, "%s\n", text_buffer[i]);
     }
 
     fclose(fp);
+    is_modified = 0; // Setelah di-save, status kembali jadi 'Saved'
+    strcpy(current_filename, filename);
+    
     printf("[INFO] File berhasil disimpan ke %s\n", filename);
 }
 
 // Membuka file dari disk dan memuat isinya ke buffer (disk ke buffer)
+// Di file_io.c karena terkait langsung dengan operasi file, bukan logika input atau rendering.
 void openFile(const char *filename) {
     FILE *fp = fopen(filename, "r");
     
     if (fp == NULL) {
-        printf("[ERROR] File '%s' tidak ditemukan.\n", filename);
+        printf("\n[ERROR] File '%s' tidak ditemukan.\n", filename);
         return;
     }
 
-    // Kosongkan buffer dulu sebelum load file baru
     clearBuffer();
+    strcpy(current_filename, filename); // Catat nama filenya
 
     char line[MAX_COL];
     while (fgets(line, sizeof(line), fp) != NULL) {
-        line[strcspn(line, "\n")] = 0; // hapus \n
-        appendLine(line);              // masukkan ke buffer Putra
+        line[strcspn(line, "\r\n")] = 0;
+        appendLine(line);
     }
 
     fclose(fp);
-    printf("[INFO] File '%s' berhasil dibuka. %d baris dimuat.\n", filename, total_lines);
+    is_modified = 0; // Baru dibuka, jadi belum ada perubahan (Saved)
+    
+    printf("\n[INFO] %s terbuka. Status: buffer terisi [%d Baris]\n", filename, total_lines);
+    getch();
 }
--- file_io.h
+++ file_io.h
@@ -1,6 +1,10 @@
 #ifndef FILE_IO_H
 #define FILE_IO_H
 
+// Deklarasi variabel global untuk status modifikasi dan nama file saat ini, untuk digunakan di file lain seperti render.c
+extern int is_modified;
+extern char current_filename[256];
+
 // Menyimpan isi buffer ke file .txt
 void saveToFile(const char *filename);
 
